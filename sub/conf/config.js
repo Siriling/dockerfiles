cf95e357d690944913b5e483f1f1fbd03d96728f
ling <1042585959@qq.com>
2023-03-26 16:51:40 +0800

    更新配置

---
+++ sub/conf/config.js
@@ -0,0 +1,22 @@
+window.config = {
+  siteName: 'Subconverter Web',
+  apiUrl: 'http://127.0.0.1:25500',
+  shortUrl: 'https://url.siriling.com:81',
+  menuItem: [
+    {
+      title: '首页',
+      link: '/',
+      target: '',
+    },
+    {
+      title: '短链接',
+      link: 'https://url.siriling.com:81',
+      target: '_blank',
+    },
+    {
+      title: 'GitHub',
+      link: 'https://github.com/Siriling/sub-web',
+      target: '_blank',
+    },
+  ],
+};
